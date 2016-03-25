389ed55d82bff039ade1d4206f8edfc66eb33a10
litwrd <litwrd@163.com>
2016-03-25 16:42:25 +0800

    add php study

---
+++ php/study/edit.php
@@ -0,0 +1,7 @@
+<?php
+include 'vars.php';
+
+echo "<input value='首页'></input> ";
+echo "<br> ";
+echo "<input value='" . $car . "'></input> ";
+?>
---
+++ php/study/test1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>测试</h1>
+<div class="menu">
+<?php include 'menu.php';?>
+</div>
+<?php
+echo "Hello World!";
+?>
+<br>
+<?php
+include 'vars.php';
+echo "我有一辆" . $color . $car ;
+?>
+<br>
+
+<?php
+echo "file noFileExists.php is not exists";
+#require 'noFileExists.php';
+include 'noFileExists.php';
+echo "I have a $color $car.";
+?>
+
+<br>
+<?php include 'edit.php';?>
+<br>
+<?php include 'footer.php';?>
+</body>
+</html>
