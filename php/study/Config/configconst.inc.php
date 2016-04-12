ad630752562d900328ea2faf9d59fc6aac9da3a2
litwrd <litwrd@163.com>
2016-04-12 17:09:34 +0800

    update php

--- php/study/Config/configconst.inc.php
+++ php/study/Config/configconst.inc.php
@@ -6,3 +6,22 @@ if (!defined('ROOT_PATH'))
 */
 
 define('TESTVAR1', 0.01);
+
+// PHP 索引数组
+/*
+ * $cars=array("Volvo","BMW","SAAB");
+ * $cars[0]="Volvo";
+ */
+
+// PHP 关联数组
+/*
+ * $test_v2 = $test_def1['testvar_2']; 
+ * echo $test_v2['testv_2_1'];
+ */
+$test_def1 = array(
+		'testvar_1' => '/user/login.html', //登录页
+        'testvar_2' => array(
+           'testv_2_1' => 'phone_code_session', //短信验证码keyname
+           'testv_2_2' => 600, //保存 单位秒 600表示缓存600秒
+    ),
+);
--- php/study/Tests/test1.php
+++ php/study/Tests/test1.php
@@ -5,6 +5,11 @@
 </head>
 <body>
 
+<?php 
+//if (!defined('ROOT_PATH'))
+//	die('不能访问'); //防止直接访问
+?>
+	
 <h1>测试</h1>
 <div class="menu">
 <?php include 'menu.php';?>
@@ -28,10 +33,19 @@ echo "I have a $color $car.";
 
 <br>
 <?php include 'edit.php';?>
-<br>
-<?php include 'footer.php';?>
 
 <?php include '../Config/configconst.inc.php';
-echo constant("TESTVAR1");?>
+echo constant("TESTVAR1");
+echo "<br>";
+echo $test_def1['testvar_1'];
+echo "<br>";
+$test_v2 = $test_def1['testvar_2']; 
+echo $test_v2['testv_2_1'];
+echo "<br>";
+echo $test_v2['testv_2_2'];
+?>
+
+<br>
+<?php include 'footer.php';?>
 </body>
 </html>
