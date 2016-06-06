310b973850b984b51a7b856307fe1ff1ee7a3d99
litwrd <litwrd@github.com>
2016-06-06 15:16:08 +0800

    update eshop app

--- php/v0000/phpstudy/apps/eshop/index.php
+++ php/v0000/phpstudy/apps/eshop/index.php
@@ -1,2 +1,11 @@
 <?php 
+echo defined("APP");
+echo "<br>";
+if (0 != defined("APP")) {
+    echo constant("APP");
+ } else {
+     echo "NO APP";
+ }
+echo "<br>";
+echo "good";
 ?>
--- php/v0000/phpstudy/index.php
+++ php/v0000/phpstudy/index.php
@@ -1,18 +1,23 @@
 <?php 
-   $_DocumentPath = $_SERVER['DOCUMENT_ROOT'];
+   //$_DocumentPath = $_SERVER['DOCUMENT_ROOT'];
    //var_dump($_DocumentPath); // "E:/workspace_php/phpstudy"
     
-   $_FilePath = __FILE__;
+   //$_FilePath = __FILE__;
    //var_dump($_FilePath); // "E:\workspace_php\phpstudy\index.php"
    
    //$_RequestUri = $_SERVER['REQUEST_URI'];
    //var_dump($_RequestUri);  // "/"
    
-   $_AppPath = str_replace($_DocumentPath, '', $_FilePath);    //==>\router\index.php
+   //$_AppPath = str_replace($_DocumentPath, '', $_FilePath);    //==>\router\index.php
    //var_dump($_AppPath); // "E:\workspace_php\phpstudy\index.php"
    
    // 这里就是重定向
    //$module_file = ".\\test\\test1.php";
+
+   ini_set('session.gc_maxlifetime', '86400');     //设置Session最大超时时间为一天
+   define("APP","eshop");
+   
    $module_file = ".\\test\\router.php";
+   $module_file = ".\\apps\\eshop\\index.php";
    include $module_file;
 ?>
